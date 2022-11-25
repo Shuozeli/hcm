8f29996c1c07b59e53015fd8a91a6141e4f66635
Shuoze Li <szsuzyli@gmail.com>
2022-11-25 15:33:46 -0500

    initial commit

---
+++ src/main/java/com/shuozeli/hcm/HumanCapitalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.shuozeli.hcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanCapitalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanCapitalManagementApplication.class, args);
+	}
+
+}
